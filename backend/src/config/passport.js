bc19814064159102bb62631c8adf2cf1f4723f56
SaptakBhattacharyya <saptakbhattacharyya16@gmail.com>
2026-04-29 15:29:30 +0530

    fix: add proxy trust to passport google strategy for vercel

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -8,6 +8,7 @@ module.exports = function (passport) {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/api/v1/users/auth/google/callback',
+        proxy: true,
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
